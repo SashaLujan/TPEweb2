a21d785da58c18aa117f24045791fe2e444416cf
sasha <lujannatasha1989@gmail.com>
2024-10-08 19:54:08 -0300

    se acomodo error de formulario

--- templates_c/f521622ddefd43eea64ad6be118747dce376dae2_0.file.formUser.tpl.php
+++ templates_c/f521622ddefd43eea64ad6be118747dce376dae2_0.file.formUser.tpl.php
@@ -1,4 +1,30 @@
-{include 'templates/header.tpl'}
+<?php
+/* Smarty version 3.1.34-dev-7, created on 2024-10-09 00:52:55
+  from 'C:\xampp\htdocs\TPEweb2\templates\formUser.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.34-dev-7',
+  'unifunc' => 'content_6705b7c73956e2_77311050',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'f521622ddefd43eea64ad6be118747dce376dae2' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\TPEweb2\\templates\\formUser.tpl',
+      1 => 1728427970,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+    'file:templates/header.tpl' => 1,
+    'file:templates/footer.tpl' => 1,
+  ),
+),false)) {
+function content_6705b7c73956e2_77311050 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_subTemplateRender('file:templates/header.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+?>
     <div>
         <h4 class="text-center" class="blockquote">Suscribirse</h4>
         <form class="row g-3" action="guardar_usuario" method="POST">
@@ -25,4 +51,6 @@
             </div>
         </form>
     </div>
-{include 'templates/footer.tpl'}
+<?php $_smarty_tpl->_subTemplateRender('file:templates/footer.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+}
+}
